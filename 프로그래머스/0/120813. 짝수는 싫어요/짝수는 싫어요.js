f9a72b82e41bcbfd7b8b3a9e3663dc7aa9e2286b
민우석 <132043249+ws-071211@users.noreply.github.com>
2024-04-30 22:56:24 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,8 +1,7 @@
 function solution(n) {
     let res = [];
     while(n>=0){
-        n % 2 != 0 ? res.unshift(n) : null
+        n % 2 != 0 ? res.push(n) : null
         n -= 1;
     }
-    return res;
 }
